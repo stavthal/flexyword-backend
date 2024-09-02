4eeb03526b1790cf1e30f8ef2c349a8bd92be819
Stavros Thalassinos <stavros8003@gmail.com>
2024-09-03 02:47:59 +0300

    changes to user model

--- models/user.go
+++ models/user.go
@@ -1,21 +1,16 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	ID       int    `json:"id" gorm:"primary_key"`
 	Username string `json:"username" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
 	Email    string `json:"email" gorm:"unique;not null"`
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
 	BillingAddress string `json:"billing_address" gorm:"nullable"`
 
 	// TODO: Add a field to store subscription tier and billing details
